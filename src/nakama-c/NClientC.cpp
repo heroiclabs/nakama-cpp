93141eade313ab17b7ff687ff60f7f244fb27052
dimon4eg <kdl.dima@gmail.com>
2019-09-22 20:50:07 +0300

    linkCustom

--- src/nakama-c/NClientC.cpp
+++ src/nakama-c/NClientC.cpp
@@ -322,7 +322,14 @@ void NClient_linkSteam(NClient client, NSession session, const char* token, NCli
 
 void NClient_linkCustom(NClient client, NSession session, const char* id, NClientReqData reqData, void (*successCallback)(NClient, NClientReqData), NClientErrorCallback errorCallback)
 {
-    
+    Nakama::NClientInterface* cppClient = getCppClient(client);
+    auto cppSession = Nakama::getSession(session);
+
+    cppClient->linkCustom(
+        cppSession,
+        id,
+        Nakama::createOkEmptyCallback(client, reqData, successCallback),
+        Nakama::createErrorCallback(client, reqData, errorCallback));
 }
 
 void NClient_unlinkFacebook(NClient client, NSession session, const char* accessToken, NClientReqData reqData, void (*successCallback)(NClient, NClientReqData), NClientErrorCallback errorCallback)
