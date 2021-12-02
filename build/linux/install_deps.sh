2aa445e63a8c03d2236f3e31bd6f83a10c1706af
Adam Comerford <adam@comerford.net>
2021-12-02 03:53:04 +0000

    Update boost download URL (bintray decom) (#45)
    Same version, just a new URL to get the source. Tested and compiles on Ubuntu 18.04

--- build/linux/install_deps.sh
+++ build/linux/install_deps.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y build-essential autoconf libtool pkg-config wget
 sudo apt-get install -y libgflags-dev libgtest-dev
 sudo apt-get install -y clang libc++-dev golang perl
 mkdir -p build && cd build
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+wget https://boostorg.jfrog.io/artifactory/main/release/1.69.0/source/boost_1_69_0.tar.gz
 tar xvzf boost_1_69_0.tar.gz
 cd boost_1_69_0
 ./bootstrap.sh --with-libraries=system,chrono,thread
