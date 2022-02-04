6bacdeac2377c3f31bb3efa0fcf828b6c4174904
Jaeho Shim <azurepine@gmail.com>
2022-02-04 16:35:05 +0900

    Added 'C++ standard' build option for Windows / VS2022 support

--- build/windows/build_windows.py
+++ build/windows/build_windows.py
@@ -29,14 +29,16 @@ init_common(os.path.abspath('..'), 'windows')
 parser = argparse.ArgumentParser(description='builder for Windows')
 parser.add_argument('-m', '--mode', help='build mode: Debug or Release')
 parser.add_argument('-a', '--arch', help='architecture: x86 or x64')
-parser.add_argument('-t', '--tool', help='platform toolset: v140, v141...')
+parser.add_argument('-t', '--tool', help='platform toolset: v140, v141, v142, v143...')
+parser.add_argument('-s', '--stdcpp', help='C++ standard: 11, 14, 17, 20...')
 parser.add_argument(      '--dll',  help='build DLL', action='store_true')
 
 args = parser.parse_args()
 
 BUILD_MODE = 'Debug'
-ARCH = 'x64'
-TOOLSET = args.tool
+ARCH = 'x86'
+TOOLSET = 'v142'
+CXX_STANDARD = ''
 DLL = args.dll
 
 if args.mode:
@@ -53,6 +55,22 @@ if args.arch:
         ARCH = args.arch
     else:
         print('Not valid architecture. Supported values:', str(valid_archs))
+        sys.exit(-1)
+
+if args.tool:
+    valid_toolsets = ['v140', 'v141', 'v142', 'v143']
+    if args.tool in valid_toolsets:
+        TOOLSET = args.tool
+    else:
+        print('Not valid toolset. Supported values:', str(valid_toolsets))
+        sys.exit(-1)
+
+if args.stdcpp:
+    valid_stdcpps = ['11', '14', '17', '20']
+    if args.stdcpp in valid_stdcpps:
+        CXX_STANDARD = args.stdcpp
+    else:
+        print('Not valid C++ standard. Supported values:', str(valid_stdcpps))
         sys.exit(-1)
 
 if BUILD_MODE == 'Debug':
@@ -88,15 +106,17 @@ def copy_dll(dest):
     copy_file(BUILD_DIR + '\\src\\' + BUILD_MODE + '\\nakama-cpp' + libs_postfix + '.dll', dest)
 
 # generate Visual Studio projects
-#generator, vs_year, default_toolset = 'Visual Studio 14 2015', 2015, 'v140'
-#generator, vs_year, default_toolset = 'Visual Studio 15 2017', 2017, 'v141'
-generator, vs_year, default_toolset = 'Visual Studio 16 2019', 2019, 'v142'
+if TOOLSET == 'v140':
+    generator, vs_year = 'Visual Studio 14 2015', 2015
+elif TOOLSET == 'v141':
+    generator, vs_year = 'Visual Studio 15 2017', 2017
+elif TOOLSET == 'v142':
+    generator, vs_year = 'Visual Studio 16 2019', 2019
+elif TOOLSET == 'v143':
+    generator, vs_year = 'Visual Studio 17 2022', 2022
 
 if ARCH == 'x64' and vs_year < 2019:
     generator += ' Win64'
-
-if not TOOLSET:
-    TOOLSET = default_toolset
 
 set_build_folder_name(TOOLSET + '_' + ARCH)
 
@@ -123,6 +143,9 @@ cmake_cmd = ['cmake',
   '-T', TOOLSET,
   '-DCMAKE_SYSTEM_VERSION=' + SYSTEM_VERSION
 ]
+
+if CXX_STANDARD != '':
+    cmake_cmd.extend(['-DCMAKE_CXX_STANDARD=' + CXX_STANDARD])
 
 cmake_cmd.extend(get_common_cmake_parameters(DLL))
 
