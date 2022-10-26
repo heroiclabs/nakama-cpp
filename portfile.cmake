180e0e632096eb0fa057f78cac58267152a5083f
Luke Gehorsam <luke@heroiclabs.com>
2022-10-26 16:02:43 -0400

    now provide the actual hash

--- portfile.cmake
+++ portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO heroiclabs/nakama-cpp
     REF 9f984507326a1e60a9adb785cbf455abc8202bf9
-    SHA512 0
+    SHA512 05446f2a59947afcfd029be62c7639566765f2dc1564982b6d5366ebf60f4be6a4539d1caaa0ab20d27bb8b4a806862ddcb1d5bd0007b7a28eb26e0f10bf0164
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
