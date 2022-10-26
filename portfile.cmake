742992876c556bd93635985ee6d512b9a38fe37a
Luke Gehorsam <luke@heroiclabs.com>
2022-10-26 13:18:41 -0400

    update REF

--- portfile.cmake
+++ portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO heroiclabs/nakama-cpp
-    HEAD_REF master
+    REF 9f984507326a1e60a9adb785cbf455abc8202bf9
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
