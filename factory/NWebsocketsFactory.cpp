d507c3487578e9b83c6fc3ba6aa5590463e00084
Luke Gehorsam <luke@heroiclabs.com>
2022-11-03 12:10:36 -0400

    proper use of BUILD_CURL flag

--- factory/NWebsocketsFactory.cpp
+++ factory/NWebsocketsFactory.cpp
@@ -22,7 +22,9 @@
 #include "../../impl/wsLibHttpClient/NWebsocketLibHC.h"
 #elif defined(BUILD_WEBSOCKET_WSLAY)
 #include "NWebsocketWslay.h"
+    #if defined(BUILD_CURL)
 #include "NetIOCurl.h"
+    #endif
 #endif
 
 namespace Nakama {
@@ -32,7 +34,7 @@ NRtTransportPtr createDefaultWebsocket(const NPlatformParameters& platformParams
     (void)platformParams;  // silence unused variable warning on some platforms
 #if defined(BUILD_WEBSOCKET_LIBHTTPCLIENT)
     return NRtTransportPtr(NWebsocketLibHC::New(platformParams));
-#elif defined(BUILD_WEBSOCKET_WSLAY)
+#elif defined(BUILD_WEBSOCKET_WSLAY) && defined(BUILD_CURL)
     return NRtTransportPtr(new NWebsocketWslay<NetIOCurl>());
 #endif
 }
