fdefe924f530900f4d263dc9e6b728a48e6d84c4
Luke Gehorsam <luke@heroiclabs.com>
2022-12-03 12:49:16 -0500

    remove in-source libhttpclient

--- third_party/libHttpClient/Source/HTTP/Apple/session_delegate.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#pragma once
-#import <Foundation/Foundation.h>
-
-@interface SessionDelegate : NSObject<NSURLSessionTaskDelegate, NSURLSessionDataDelegate>
-+ (SessionDelegate*) sessionDelegateWithHCCallHandle:(HCCallHandle) call andCompletionHandler:(void(^)(NSURLResponse* response, NSError* error)) completion;
-@end
