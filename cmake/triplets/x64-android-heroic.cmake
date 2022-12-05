fa0aaaad945bfa793371cb479b36d1173cf93a05
Luke Gehorsam <luke@heroiclabs.com>
2022-12-05 17:56:26 -0500

    remove chainloads from triplet files

--- cmake/triplets/x64-android-heroic.cmake
+++ cmake/triplets/x64-android-heroic.cmake
@@ -4,9 +4,3 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 
 set(VCPKG_BUILD_TYPE release)
-
-# toolchain file is included in the NDK, so we can't predefine path to it
-if(NOT DEFINED ENV{VCPKG_CHAINLOAD_TOOLCHAIN_FILE})
-    message(FATAL_ERROR "Pass VCPKG_CHAINLOAD_TOOLCHAIN_FILE env")
-endif()
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{VCPKG_CHAINLOAD_TOOLCHAIN_FILE}")
