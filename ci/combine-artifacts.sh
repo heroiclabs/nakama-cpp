6e36a27883057f9fe8eaf5631de88a15482c1815
Luke Gehorsam <luke@heroiclabs.com>
2023-01-19 08:58:52 -0500

    remove unreal-specific scripts from ci

--- ci/combine-artifacts.sh
+++ ci/combine-artifacts.sh
@@ -13,26 +13,6 @@ find_git_sha() {
   git_sha="${one##*-git.}"
 }
 
-prep_unreal_osx_universal() {
-  echo '::group::Prepare Unreal OSX universal binaries'
-  # output dir is in artifacts dir and name is chosen so that combine_unreal finds it
-  local out=${artifacts_dir}/libnakama-Unreal-macosx-universal-git.${git_sha}
-  mkdir -p ${out}
-
-  # Everything, but library should be identical. Copy one arch and then delete lib
-  # TODO: dont expect arm64 to always be present, pick first match instead
-  cp -rf ${artifacts_dir}/libnakama-Unreal-macosx-arm64*-git.${git_sha}/* ${out}/
-  rm -rf ${out}/Nakama/libnakama/macosx-arm64/nakama-sdk.framework/Versions/A/nakama-sdk
-  mv ${out}/Nakama/libnakama/macosx-{arm64,universal}
-
-  lipo -create \
-    -o ${out}/Nakama/libnakama/macosx-universal/nakama-sdk.framework/Versions/A/nakama-sdk \
-    ${artifacts_dir}/libnakama-Unreal-macosx-*/Nakama/libnakama/*/nakama-sdk.framework/Versions/A/nakama-sdk
-  # delete original artifacts so that combine_unreal doesn't find them
-  rm -rf ${artifacts_dir}/libnakama-Unreal-macosx-!(universal)-*-git.${git_sha}
-  echo '::endgroup::'
-}
-
 prep_osx_universal() {
   echo '::group::Prepare generic OSX universal binaries'
   local out=${artifacts_dir}/libnakama-macosx-universal-git.${git_sha}
@@ -48,17 +28,6 @@ prep_osx_universal() {
     ${artifacts_dir}/libnakama-macosx-*/*/nakama-sdk.framework/Versions/A/nakama-sdk
 
   rm -rf ${artifacts_dir}/libnakama-macosx-!(universal)-*-git.${git_sha}
-  echo '::endgroup::'
-}
-
-combine_unreal() {
-  echo '::group::Combine Unreal'
-  local out=libnakama-Unreal-git.${git_sha}
-  mkdir -p ${out}
-  cp -rf ${artifacts_dir}/libnakama-Unreal-*/* ${out}/
-  rm -rf ${artifacts_dir}/libnakama-Unreal-*-git.${git_sha}
-  echo "::set-output name=unreal-artifact-name::${out}"
-  echo "::set-output name=unreal-artifact-dir::${out}"
   echo '::endgroup::'
 }
 
@@ -83,8 +52,6 @@ post_run_checks() {
 }
 
 find_git_sha
-prep_unreal_osx_universal
 prep_osx_universal
-combine_unreal
 combine_generic
 post_run_checks
