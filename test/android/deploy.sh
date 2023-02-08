d4bf6cb0e6d12cf086daf59b26298698deea1bf0
Luke Gehorsam <luke@heroiclabs.com>
2023-02-08 15:34:30 -0500

    fix deploy script

--- test/android/deploy.sh
+++ test/android/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-adb -s "$0" uninstall com.heroiclabs.nakamatest
-adb is "$0" install ./build/outputs/apk/debug/nakamatest-debug.apk
+adb -s "$1" uninstall com.heroiclabs.nakamatest
+adb -s "$1" install ./build/outputs/apk/debug/nakamatest-debug.apk
 adb logcat -c
 adb shell am start -a android.intent.action.MAIN -n com.heroiclabs.nakamatest/android.app.NativeActivity
 adb logcat
