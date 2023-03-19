8d8e59011dc717cf86d28b61779edd5e65ae222c
Luke Gehorsam <luke@heroiclabs.com>
2023-03-19 15:03:05 -0400

    Add async headers to rtclient

--- core/core-rt/NRtClient.h
+++ core/core-rt/NRtClient.h
@@ -196,6 +196,112 @@ namespace Nakama {
 
         void sendPartyData(const std::string& partyId, long opCode, NBytes& data) override;
 
+        std::future<NChannelPtr> joinChatAsync(
+            const std::string& target,
+            NChannelType type,
+            const opt::optional<bool>& persistence = opt::nullopt,
+            const opt::optional<bool>& hidden = opt::nullopt
+        ) override;
+
+        std::future<void> leaveChatAsync(
+            const std::string& channelId,
+            std::function<void()> successCallback = nullptr,
+            RtErrorCallback errorCallback = nullptr
+        ) override;
+
+        std::future<const NChannelMessageAck&> writeChatMessageAsync(
+            const std::string& channelId,
+            const std::string& content
+        ) override;
+
+        std::future<const NChannelMessageAck&> updateChatMessageAsync(
+            const std::string& channelId,
+            const std::string& messageId,
+            const std::string& content
+        ) override;
+
+        std::future<void> removeChatMessageAsync(
+            const std::string& channelId,
+            const std::string& messageId
+        ) override;
+
+        std::future<const NMatch&> createMatchAsync() override;
+
+        std::future<const NMatch&> joinMatchAsync(
+            const std::string& matchId,
+            const NStringMap& metadata
+        ) override;
+
+        std::future<const NMatch&> joinMatchByTokenAsync(
+            const std::string& token
+        ) override;
+
+        std::future<void> leaveMatchAsync(
+            const std::string& matchId
+        ) override;
+
+        std::future<const NMatchmakerTicket&> addMatchmakerAsync(
+            const opt::optional<int32_t>& minCount = opt::nullopt,
+            const opt::optional<int32_t>& maxCount = opt::nullopt,
+            const opt::optional<std::string>& query = opt::nullopt,
+            const NStringMap& stringProperties = {},
+            const NStringDoubleMap& numericProperties = {},
+            const opt::optional<int32_t>& countMultiple = opt::nullopt
+        ) override;
+
+        std::future<void> removeMatchmakerAsync(
+            const std::string& ticket
+        ) override;
+
+        std::future<void> sendMatchDataAsync(
+            const std::string& matchId,
+            std::int64_t opCode,
+            const NBytes& data,
+            const std::vector<NUserPresence>& presences = {}
+        ) override;
+
+        std::future<const NStatus&> followUsersAsync(
+            const std::vector<std::string>& userIds
+        ) override;
+
+        std::future<void> unfollowUsersAsync(
+            const std::vector<std::string>& userIds
+        ) override;
+
+        std::future<void> updateStatusAsync(
+            const std::string& status
+        ) override;
+
+        std::future<const NRpc&> rpcAsync(
+            const std::string& id,
+            const opt::optional<std::string>& payload = opt::nullopt,
+            std::function<void(const NRpc&)> successCallback = nullptr,
+            RtErrorCallback errorCallback = nullptr
+        ) override;
+
+        std::future<void> acceptPartyMemberAsync(const std::string& partyId, NUserPresence& presence) override;
+
+        std::future<const NPartyMatchmakerTicket&> addMatchmakerPartyAsync(const std::string& partyId, const std::string& query, int32_t minCount, int32_t maxCount,
+            const NStringMap& stringProperties = {}, const NStringDoubleMap& numericProperties = {},
+            const opt::optional<int32_t>& countMultiple = opt::nullopt) override;
+
+        std::future<void> closePartyAsync(const std::string& partyId) override;
+
+        std::future<const NParty&> createPartyAsync(bool open, int maxSize) override;
+
+        std::future<void> joinPartyAsync(const std::string& partyId) override;
+
+        std::future<void> leavePartyAsync(const std::string& partyId, std::function<void()> successCallback = nullptr, RtErrorCallback errorCallback = nullptr) override;
+
+        std::future<const NPartyJoinRequest&> listPartyJoinRequestsAsync(const std::string& partyId) override;
+
+        std::future<void> promotePartyMemberAsync(const std::string& partyId, NUserPresence& partyMember) override;
+
+        std::future<void> removeMatchmakerPartyAsync(const std::string& partyId, const std::string& ticket) override;
+
+        std::future<void> removePartyMemberAsync(const std::string& partyId, NUserPresence& presence) override;
+
+        std::future<void> sendPartyDataAsync(const std::string& partyId, long opCode, NBytes& data) override;
 
         protected:
             void onTransportDisconnected(const NRtClientDisconnectInfo& info);
