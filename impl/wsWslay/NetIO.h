a217fa66d7d1d55c762d4d88f6d4324b05d8541b
Luke Gehorsam <luke@heroiclabs.com>
2023-05-11 14:17:30 -0400

    fix compiler directives around createRtClient

--- impl/wsWslay/NetIO.h
+++ impl/wsWslay/NetIO.h
@@ -4,7 +4,7 @@ namespace Nakama {
 
 // Result of async operation
 enum class NetIOAsyncResult {
-    ERROR = -1,  // error, no further progress possible
+    ERR = -1,  // error, no further progress possible
     AGAIN = 0,   // to be called again
     DONE = 1,    // successful completion
 };
