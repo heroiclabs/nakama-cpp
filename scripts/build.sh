f30afb981168396da049b77d563098a5576dd1c3
Luke Gehorsam <luke@heroiclabs.com>
2024-03-07 14:52:29 -0500

    tty progress

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec docker buildx build --platform linux/amd64 -f $(dirname $0)/Dockerfile --progress=plain $(dirname $0)/.. --output ${1:?pass output directory as first argument}
+exec docker buildx build --platform linux/amd64 -f $(dirname $0)/Dockerfile --progress=tty $(dirname $0)/.. --output ${1:?pass output directory as first argument}
