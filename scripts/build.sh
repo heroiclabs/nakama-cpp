c6bd35bda62c3547390bae1eb1c3da6fed23417c
Luke Gehorsam <luke@heroiclabs.com>
2024-03-07 12:57:59 -0500

    cleanup dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-
-
-exec docker build -f $(dirname $0)/Dockerfile --progress=plain $(dirname $0)/.. --output ${1:?pass output directory as first argument}
+exec docker buildx build --platform linux/amd64 -f $(dirname $0)/Dockerfile --progress=plain $(dirname $0)/.. --output ${1:?pass output directory as first argument}
