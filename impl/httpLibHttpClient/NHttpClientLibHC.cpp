aa3b2fcc4df884d1d91063073ab47ab58f843771
Maxim Ivanov <maxim@heroiclabs.com>
2024-10-03 14:00:51 +0100

    libhttpclient build fixes

--- impl/httpLibHttpClient/NHttpClientLibHC.cpp
+++ impl/httpLibHttpClient/NHttpClientLibHC.cpp
@@ -1,10 +1,24 @@
-//
-// Created by yamlcoder on 17/03/2022.
-//
+/*
+ * Copyright 2022 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "NHttpClientLibHC.h"
 
 #include <httpClient/httpClient.h>
 #include <nakama-cpp/log/NLogger.h>
-#include "NHttpClientLibHC.h"
+
 namespace Nakama {
 
 HC_DEFINE_TRACE_AREA(httpTransportLibHC, HCTraceLevel::Error);
