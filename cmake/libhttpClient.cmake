9ef76e75e1c7b762ee3a634f2eaaf4f64ecf99b6
Maxim Ivanov <maxim@heroiclabs.com>
2024-10-05 08:43:17 +0100

    Use aliases where possible for safety, cleanup libhttpclient build scripts

--- cmake/libhttpClient.cmake
+++ cmake/libhttpClient.cmake
@@ -3,60 +3,24 @@ if (LINUX OR APPLE)
     target_compile_definitions(CURL::libcurl INTERFACE CURL_STRICTER)
 endif()
 
-
-set(LIBHTTPCLIENT_OBJECT_LIBRARY TRUE)
-set(WAS_SHARED_LIBS ${BUILD_SHARED_LIBS})
-
 if (ANDROID)
+    ## force libhttpclient to build statically
+    set(WAS_SHARED_LIBS ${BUILD_SHARED_LIBS})
+    set(BUILD_SHARED_LIBS OFF)
     set(LIBHTTPCLIENT_SOURCE_SUBDIR "Build/libHttpClient.Android")
-    return()
-endif()
-
-add_subdirectory(cmake/libHttpClient)
-
-## force libhttpclient to build statically
-set(BUILD_SHARED_LIBS OFF)
-
-#add_subdirectory("${CMAKE_SOURCE_DIR}/submodules/libHttpClient/${LIBHTTPCLIENT_SOURCE_SUBDIR}")
-
-set(BUILD_SHARED_LIBS ${WAS_SHARED_LIBS})
-
-if (ANDROID)
+    add_subdirectory("${CMAKE_SOURCE_DIR}/submodules/libHttpClient/${LIBHTTPCLIENT_SOURCE_SUBDIR}")
+    set(BUILD_SHARED_LIBS ${WAS_SHARED_LIBS})
     set(LIBHTTPCLIENT_TARGET libHttpClient.Android)
-else()
-
-    if(XDK)
-        if (WINRT)
-            set(LIBHTTPCLIENT_TARGET libHttpClient.XDK.WinRT)
-        else()
-            set(LIBHTTPCLIENT_TARGET libHttpClient.XDK.C)
-        endif()
-    elseif (GDK)
-        set(LIBHTTPCLIENT_TARGET libHttpClient.GDK.C)
-        include(submodules/devkits/cmake/gdk-targets.cmake)
-        target_link_libraries(${LIBHTTPCLIENT_TARGET} PRIVATE GDK::XCurl)
-    elseif (BUILDWIN32)
-        set(LIBHTTPCLIENT_TARGET libHttpClient.Win32.C)
-    elseif (APPLE)
-        set(LIBHTTPCLIENT_TARGET libHttpClient.Apple.C)
-    elseif(DEFINED LINUX)
-        set(LIBHTTPCLIENT_TARGET libHttpClient.Linux.C)
-    else()
-        if (WINRT)
-            set(LIBHTTPCLIENT_TARGET libHttpClient.UWP.WinRT)
-        else()
-            set(LIBHTTPCLIENT_TARGET libHttpClient.UWP.C)
-        endif()
-    endif()
+else ()
+    add_subdirectory(cmake/libHttpClient)
+    set(LIBHTTPCLIENT_TARGET libHttpClient)
 endif()
-set(LIBHTTPCLIENT_TARGET "libHttpClient")
 
-# We build LIBHTTPCLIENT as OBJECT library so that its symbols
-# wont be excluded due to '--exclude-libs ALL' linker flag. Although we
-# dont expose libhttpclient in our API , we still want it's symbols exported
-# in our ABI, because that is how JVM finds native code on Android platform.
-list(APPEND NAKAMA_SDK_DEPS ${LIBHTTPCLIENT_TARGET})
+if (GDK)
+    include(submodules/devkits/cmake/gdk-targets.cmake)
+    target_link_libraries(libHttpClient PRIVATE GDK::XCurl)
+endif()
 
 if (NOT WITH_WS_LIBHTTPC)
-    target_compile_definitions(${LIBHTTPCLIENT_TARGET} PRIVATE "HC_NOWEBSOCKETS")
+    target_compile_definitions(libHttpClient PRIVATE "HC_NOWEBSOCKETS")
 endif()
