df0cca0ef8084acfa1f96160decdb988ff842bd7
Maxim Ivanov <hi+github@yamlcoder.me>
2024-10-07 13:04:42 +0100

    Fix iphonesimulator builds

--- cmake/triplets/arm64-iphonesimulator-heroic.cmake
+++ cmake/triplets/arm64-iphonesimulator-heroic.cmake
@@ -1,12 +1,12 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
 
-# Heroic Labs additions to standard triplets:
+# HeroicLabs additions to standard triplets:
 set(VCPKG_BUILD_TYPE release)
-set(VCPKG_OSX_DEPLOYMENT_TARGET "11")
-set(VCPKG_OSX_ARCHITECTURES "x86_64")
+set(VCPKG_OSX_DEPLOYMENT_TARGET "12")
+set(VCPKG_OSX_ARCHITECTURES "arm64")
 set(VCPKG_OSX_SYSROOT iphonesimulator)
 
 include(${CMAKE_CURRENT_LIST_DIR}/feature-visibility-hidden.cmake)
