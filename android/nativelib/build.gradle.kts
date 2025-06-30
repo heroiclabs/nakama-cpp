1c568982faa2965cef46e9e322ecc2ae568f8ede
Maxim Ivanov <maxim@heroiclabs.com>
2025-06-30 15:16:53 +0100

    Android WIP

--- android/nativelib/build.gradle.kts
+++ android/nativelib/build.gradle.kts
@@ -2,18 +2,23 @@ plugins {
     id("com.android.library")
 }
 
-//java {
-//    toolchain {
-//        languageVersion = JavaLanguageVersion.of(17)
-//    }
-//}
-
 android {
     namespace = "com.heroiclabs.nakama"
     compileSdk = 34
 
+    buildFeatures {
+        prefabPublishing = true
+        prefab = true
+    }
+
+    prefab {
+        create("nakama-sdk") {
+            headers = ""
+        }
+    }
+
     defaultConfig {
-        ndkVersion = "27.1.12297006"
+        ndkVersion = "27.2.12479018"
         minSdk = 28
         externalNativeBuild {
             cmake {
@@ -33,8 +38,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
 
-    dependencies {
-        implementation("androidx.annotation:annotation:1.8.2")
-    }
+dependencies {
+        implementation("androidx.annotation:annotation:1.9.1")
 }
