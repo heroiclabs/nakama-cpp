9cb8f3b4e6de19845991c7879b26e4d18ea46da4
Maxim Ivanov <maxim@heroiclabs.com>
2025-08-22 15:50:38 +0100

    Move Resource from public headers into implementation

--- interface/include/nakama-cpp/NUtils.h
+++ interface/include/nakama-cpp/NUtils.h
@@ -30,42 +30,4 @@ NAKAMA_NAMESPACE_BEGIN
  */
 NAKAMA_API NTimestamp getUnixTimestampMs();
 
-struct disarmed_t {};
-inline constexpr disarmed_t disarmed{};
-
-// like unique_ptr but for non-pointer types
-template <typename T, int (*d)(T)> class Resource {
-public:
-  explicit Resource(T v) : r(v) {}
-  explicit Resource(disarmed_t) : r(), armed(false) {}
-  ~Resource() { armed ? d(r) : 0; }
-  Resource(Resource&) = delete;
-  Resource(const Resource&) = delete;
-  Resource& operator=(const Resource&) = delete;
-  Resource& operator=(Resource&&) = delete;
-  Resource(Resource&& o) noexcept : r(o.r), armed(o.armed) {
-    o.armed = false;
-    o.r = T();
-  }
-  explicit operator bool() { return armed; }
-
-  T& get() { return r; }
-  void reset(T v) {
-    armed ? d(r) : 0;
-    armed = true;
-    r = v;
-  }
-  void reset(disarmed_t) {
-    armed ? d(r) : 0;
-    armed = false;
-    r = T();
-  }
-  // Call this only after making sure the resource is invalid or you handle destruction manually. This will not call the
-  // delete function on the resource!
-  void disarm() { armed = false; }
-
-private:
-  T r;
-  bool armed = true;
-};
 NAKAMA_NAMESPACE_END
